61a6707ed14f8eb2d819e193dd345ba25dfb0306
Adrian C. Hinrichs <adrianhinrichs@outlook.de>
2016-02-19 17:04:19 +0100

    try to fix the error, without success
    Habe mein bestes getan, hat aber nichts gebracht :-/

--- Snake/Snake.java
+++ Snake/Snake.java
@@ -29,7 +29,7 @@ public class Snake extends JPanel
     public void start(){
         while(true){
             try {
-                Thread.sleep(50);
+                Thread.sleep(250);
                 update();
                 this.repaint();
                 mainFrame.repaint();
@@ -40,16 +40,17 @@ public class Snake extends JPanel
     }
     
     public void update(){
+        int last = currend;
         currend = (currend+1)%length;
-        knots[currend][0] =(knots[currend][0] + Math.abs((int)(Math.random() * 100)) -50)%1000;
-        knots[currend][1] =(knots[currend][1] + Math.abs((int)(Math.random() * 100)) -50)%1000;
+        knots[currend][0] =(knots[last][0] + (Math.abs((int)(Math.random() * 100)) -50)%1000);
+        knots[currend][1] =(knots[last][1] + (Math.abs((int)(Math.random() * 100)) -50)%1000);
     }
 
     public void paint(Graphics g){
         System.out.println("paint");
         g.setColor(Color.black);
-        for(int i = 0; i<length-2; i++){
-            
+        for(int i = 0; i<length-1; i++){
+            System.out.println((currend +i)%length + "-->" +(currend +i +1)%length);
             g.drawLine(knots[(currend +i)%length][0], knots[(currend +i)%length][1], knots[(currend +i+1)%length][0], knots[(currend +i+1)%length][1]);
         }
         g.setColor(Color.white);
