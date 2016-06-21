68e0cc1eaf3e8ce299e6274ced38fbce99aa0c39
Benedikt Müller <Huloben@hotmail.de>
2016-06-21 11:27:22 +0200

    hab "gründliche" Kommentare in der Main eingefügt.   Erklären wie man ein neues Projekt einbindet.

--- 1vs1/src/Main.java
+++ 1vs1/src/Main.java
@@ -18,6 +18,10 @@ import javax.swing.JFrame;
  * "Umgebung" um kleine Spiele zu sammeln
  * 5 pixel Abstand zwichen allen Buttons
  * 
+ * 
+ * Um nen neuen Button bzw. miniprojekt einzubinden muss 
+ * nur der Code an den markierten stellen in der gegeben Reihenfolge v
+ * erweiter werden, siehe unten.
  */
 
 
@@ -26,12 +30,14 @@ public class Main implements  ActionListener
 	public static int WIDTH = 1000;
 	public static int HEIGHT = 500;
 	
-	public static int BUTTON_BOUNDS = 100;
-	public static int INTERSPACE_SIZE = 5;
+	public static int BUTTON_BOUNDS = 100; //Gräße der Buttons (rechteckig)
+	public static int INTERSPACE_SIZE = 5; //Platz zwichen buttons untereinander und vom rand
 	
-	public static String ICON_PATH = "icons/";
+	public static String ICON_PATH = "icons/"; //pfad in dem die Icons für auf die Buttons liegen
 	private JFrame mainMenu;
 	
+	
+	//1111111111111111111111111111111111111111111111111111111111111111111111111111
 	private JButton WASDmini;
 	private JButton Animationstester;
 	
@@ -56,6 +62,9 @@ public class Main implements  ActionListener
 	 * -> terminiert auch das hauptfenster
 	 */
 	private void addComponentstoFrame() {
+		
+		//222222222222222222222222222222222222222222222222222222222222222222222222222
+		
 		this.WASDmini = new JButton();
 		this.WASDmini.setBounds(this.getPosOfButton(1), this.getPosOfButton(1), Main.BUTTON_BOUNDS, Main.BUTTON_BOUNDS);
 		try {
@@ -66,6 +75,8 @@ public class Main implements  ActionListener
 		  }
 		this.WASDmini.addActionListener(this);
 		this.mainMenu.add(WASDmini);
+		
+		
 		
 		this.Animationstester = new JButton();
 		this.Animationstester.setBounds(this.getPosOfButton(2), this.getPosOfButton(1), Main.BUTTON_BOUNDS, Main.BUTTON_BOUNDS);
@@ -91,6 +102,9 @@ public class Main implements  ActionListener
 	
 	@Override
 	public void actionPerformed(ActionEvent ae) {
+		
+		//333333333333333333333333333333333333333333333333333333333333333333333333333333
+		
 		if(ae.getSource().equals(this.WASDmini)) {
 			new WASD_Mini();
 		}
