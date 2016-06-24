226a54a208fe3a4bc541c84cf07913c9650f2c09
ACHinrichs <adrian-corin.hinirchs@gmx.de>
2016-06-24 18:00:36 +0200

    Create a new Class for the Centipede Game

---
+++ 1vs1/src/Centipede.java
@@ -0,0 +1,46 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class Centipede implements Game, KeyListener, Runnable{
+	
+	
+	@Override
+	public void run() {
+		while(true){
+			System.out.println("Hello");
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startGame() {
+		// TODO Auto-generated method stub
+		Thread t = new Thread(this);
+		t.start();
+		while(t.isAlive()){
+			System.out.println("Alive");
+		}
+	}
+	
+	public static void main(String[] args){
+		(new Centipede()).run();
+	}
+}
